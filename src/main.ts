37e6009c04e666288e06f8cca3cb588a1d1123fe
Keven Kurz <tentoe86@gmail.com>
2018-03-26 16:35:32 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createServer } from 'restify';
+
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+const server = createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
