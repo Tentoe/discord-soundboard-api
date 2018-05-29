1e9e27559965d44b6ce915dccc1dc5abd05ba1a5
Keven Kurz <tentoe86@gmail.com>
2018-05-29 17:12:47 +0200

    deleted all files of old restify solution

--- README.md
+++
@@ -1,9 +0,0 @@
-# discord-soundboard-api
-
-.env-file
----
-- DISCORD_BOT_USER_TOKEN
-
-static folder
----
-`./build/webapp`
--- src/config.ts
+++
@@ -1,9 +0,0 @@
-import { join as pathJoin } from 'path';
-
-const token = process.env.DISCORD_BOT_USER_TOKEN || 'noTOKEN';
-const defaultVolume = 0.3;
-const dbURL = process.env.SOUNDBOARD_DB || 'mongodb://localhost/soundboard';
-
-const soundDir = pathJoin(__dirname, 'files');
-
-export { token, defaultVolume, dbURL, soundDir };
