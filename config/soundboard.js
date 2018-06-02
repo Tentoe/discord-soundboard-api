08c32b9c78575cfde2045684686573813fcb084c
Keven Kurz <tentoe86@gmail.com>
2018-06-02 19:40:10 +0200

    added token to be loaded from env var and .env file to be loaded on start
    git update-index --skip-worktree .env

--- config/soundboard.js
+++ config/soundboard.js
@@ -3,5 +3,6 @@ const path = require('path');
 exports.default = {
   soundboard: api => ({
     soundFileDir: process.env.SOUNDBOARD_SOUND_FILE_DIR || path.join(process.cwd(), 'soundfiles'),
+    discordToken: process.env.SOUNDBOARD_DISCORD_BOT_USER_TOKEN || 'noTOKEN',
   }),
 };
